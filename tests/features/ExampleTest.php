7f525c600fe00dd5b91df9419a98e54626a8b543
Ernesto N. Aides <eaides@hotmail.com>
2016-12-13 22:41:45 +0200

    reorder the tescases in folders

--- tests/features/ExampleTest.php
+++ tests/features/ExampleTest.php
@@ -1,19 +1,13 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-
-class ExampleTest extends TestCase
+class ExampleTest extends FeatureTestCase
 {
-    use DatabaseTransactions;
-
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    function test_basic_example()
     {
         $this->visit('/')
             ->see('Laravel');
